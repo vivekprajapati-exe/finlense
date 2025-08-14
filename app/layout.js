c07b4ad8e0f5aef1a079b81f78b50b0a7329fe8d
Vivek <vivek.prajapati16072006@gmail.com>
2025-08-14 17:58:12 +0530

    Setup for drawer for ui

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { AuthCheck } from "@/components/AuthCheck";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
           <main className="min-h-screen">
             {children}
           </main>
+          <Toaster/>
         <Footer />
       </body>
     </html>
