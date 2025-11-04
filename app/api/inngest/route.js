44378456f30e6665620e5b4951076adffa2ee36c
Vivek <vivek.prajapati16072006@gmail.com>
2025-11-04 21:49:16 +0530

    deployment and bug fix ready

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,14 @@
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts } from "@/lib/inngest/function";
+import { checkBudgetAlerts, generateMonthlyReports, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/function";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-        checkBudgetAlerts,
-    ],
+    checkBudgetAlerts,
+    triggerRecurringTransactions,
+    processRecurringTransaction,
+    generateMonthlyReports
+  ],
 });
--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+import arcjet from '@arcjet/next';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 // Define protected routes
@@ -6,13 +7,35 @@ const isProtectedRoute = createRouteMatcher([
   '/transaction(.*)',
   '/account(.*)',
   '/budget(.*)',
-])
+]);
 
-export default clerkMiddleware(async (auth, req) => {
+// arcjet protection
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  // characteristics: ["userId"], // Track based on Clerk userId
+  rules: [
+    // Shield protection for content and security
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "GO_HTTP", // For Inngest
+        // See the full list at https://arcjet.com/bot-list
+      ],
+    }),
+  ],
+});
+
+const clerk = clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
     await auth.protect()
   }
 })
+
+export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
